d8b82f2bed8305bfbbb51371a4c934824f58b978
thanhtran-git <thanh.tran@live.de>
2025-02-20 16:05:30 +0100

    import navbarwrapper to form

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { SessionProvider } from "next-auth/react";
+import NavbarWrapper from "@/components/navbar-wrapper";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-primary-foreground`}
       >
+        <NavbarWrapper />
         <SessionProvider>{children}</SessionProvider>
       </body>
     </html>
