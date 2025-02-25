5d156af62bcf8b1cacf8dd88735864b381381979
thanhtran-git <thanh.tran@live.de>
2025-02-25 22:21:19 +0100

    use prisma singleton

--- auth.ts
+++ auth.ts
@@ -1,8 +1,6 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
+import { prisma } from "@/lib/prisma";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [GitHub],
