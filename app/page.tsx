62cac3e74bf4124e5d147b42e019c0e53a3e73ae
thanhtran-git <thanh.tran@live.de>
2025-03-04 22:20:24 +0100

    remove gallery

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import SidebarNav from "@/components/SidebarNav";
 import ProductCard from "@/components/ProductCard";
-import { featuredProducts } from "@/utils/dummy";
 import { sideBarLinks } from "@/lib/sideBarLinks";
 import slugify from "slugify";
 import { ClassifiedAd } from "@/lib/types";
@@ -38,22 +37,6 @@ export default async function Home() {
           <SidebarNav items={sideBarLinks} className="sticky top-24" />
         </aside>
         <main className="space-y-6">
-          <section>
-            <div className="flex items-center justify-between">
-              <h2 className="text-2xl font-bold">Galerie</h2>
-            </div>
-            <div className="mt-4 grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-              {featuredProducts.map((product) => (
-                <div key={product.id}>
-                  <ProductCard
-                    {...product}
-                    link={`/product/mock/${product.id}`}
-                  />
-                </div>
-              ))}
-            </div>
-          </section>
-
           <section>
             <div className="flex items-center justify-between">
               <h2 className="text-2xl font-bold">Neueste Anzeigen</h2>
